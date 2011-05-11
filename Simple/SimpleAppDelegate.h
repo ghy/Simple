aee2260c99dcf4e5c678337a12f7d8181092d4c8
YouMay <user@usermatoiMac.local>
2011-05-11 09:39:49 +0800

    Initial Commit

---
+++ Simple/SimpleAppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  SimpleAppDelegate.h
+//  Simple
+//
+//  Created by YouMay on 11-5-11.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SimpleAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Simple/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Simple
+//
+//  Created by YouMay on 11-5-11.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
